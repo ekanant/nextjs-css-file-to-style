31d34455526e552c586e656d5d9ecb4213fac6b6
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-06-03 00:05:02 +0700

    Add html with bootstrap css

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
-const Page = () => (<div>Hello world</div>);
+const Page = () => (
+  <>
+    <div
+      className="bg-dark"
+      style={{
+        width: '100%',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        height: '100vh',
+        zIndex: -2,
+      }}
+    />
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          This is in container
+        </div>
+      </div>
+    </div>
+  </>
+);
 
 export default Page;
